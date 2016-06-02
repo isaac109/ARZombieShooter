6135d710a1cbb33cff905232cd98064e8f034746
isaac109 <isaac109@mail.chapman.edu>
2016-06-01 17:56:45 -0700

    Db Infrastructure is added

---
+++ ARZombieShooter/Assets/Assets/Scripts/HighScoreManager.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HighScoreManager : MonoBehaviour {
+
+    private string connection;
+    private string p = "ARShooter.db";
+    private IDbConnection dbcon;
+    private IDbCommand dbcmd;
+    private IDataReader reader;
+
+	// Use this for initialization
+	void Start () {
+        OpenDB();
+        CreateTable();
+        CloseDB();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OpenDB()
+    {
+        connection = "URI=file:" + Application.persistentDataPath + "/" + p;
+        dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+    }
+
+    public void CloseDB()
+    {
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbcon.Close();
+        dbcon = null;
+    }
+
+    bool CreateTable()
+    { // Create a table, name, column array, column type array
+        string query =  "CREATE  TABLE  IF NOT EXISTS \"HighScores\" (\"PlayerID\" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , \"Name\" TEXT NOT NULL , \"Date_Time\" DATETIME NOT NULL  DEFAULT CURRENT_DATE, \"Shots\" INTEGER NOT NULL , \"Kills\" INTEGER NOT NULL , \"Level\" INTEGER NOT NULL , \"Score\" INTEGER NOT NULL )";
+        try
+        {
+            dbcmd = dbcon.CreateCommand(); // create empty command
+            dbcmd.CommandText = query; // fill the command
+            reader = dbcmd.ExecuteReader(); // execute command which returns a reader
+        }
+        catch (Exception e)
+        {
+
+            Debug.Log(e);
+            return false;
+        }
+        return true;
+    }
+
+    public void InsertScore(string name, int score, int ShotsFired, int Kills, int Level)
+    {
+        OpenDB();
+        using (dbcmd = dbcon.CreateCommand())
+        {
+            string sqlQuery = String.Format("INSERT INTO HighScores(Name,Score,Shots,Kills,Level) VALUES(\"{0}\",\"{1}\",\"{2}\",\"{3}\",\"{4}\")", name, score, ShotsFired, Kills, Level);
+            dbcmd.CommandText = sqlQuery;
+            dbcmd.ExecuteScalar();
+            CloseDB();
+        }
+    }
+
+    public List<ScoreCard> GetScores()
+    {
+        List<ScoreCard> scores = new List<ScoreCard>();
+        OpenDB();
+        using (dbcmd = dbcon.CreateCommand())
+        {
+            string sqlQuery = "SELECT * FROM HighScores ORDER BY Score DESC LIMIT 9";
+            dbcmd.CommandText = sqlQuery;
+
+            using (reader = dbcmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    ScoreCard temp = new ScoreCard(reader.GetString(1), reader.GetInt32(6));
+                    scores.Add(temp);
+                }
+                CloseDB();
+            }
+        }
+        return scores;
+    }
+}
+
+public class ScoreCard
+{
+    public string name;
+    public int score;
+    public ScoreCard(string _name, int _score)
+    {
+        name = _name;
+        score = _score;
+    }
+}
--- ARZombieShooter/Assets/Assets/Scripts/MenuHandler.cs
+++ ARZombieShooter/Assets/Assets/Scripts/MenuHandler.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MenuHandler : MonoBehaviour {
 
@@ -9,6 +10,8 @@ public class MenuHandler : MonoBehaviour {
     public GameObject hsInputScreen;
     public PlayerInfo playerInfo;
     public InputField nameField;
+    public HighScoreManager hsManager;
+    public GameObject scoreHolder;
 
     public void startGame()
     {
@@ -31,6 +34,63 @@ public class MenuHandler : MonoBehaviour {
         {
             hsInputScreen.SetActive(true);
         }
+    }
+
+    public void logInfo()
+    {
+        if (nameField.text != "")
+        {
+            hsManager.InsertScore(nameField.text, (int)playerInfo.score, (int)playerInfo.shotsFired, (int)playerInfo.kills, (int)playerInfo.lvl);
+            hasLogged = true;
+            hsInputScreen.SetActive(false);
+        }
+
+    }
+
+    public void showHighScores()
+    {
+        List<ScoreCard> scores = hsManager.GetScores();
+        int counter = 0;
+        hsInputScreen.SetActive(true);
+        for (int j = 1; j > -2; j--)
+        {
+            for (int i = -1; i < 2; i++)
+            {
+                GameObject temp = Instantiate(scoreHolder.gameObject) as GameObject;
+                temp.transform.parent = hsInputScreen.transform;
+                temp.GetComponent<RectTransform>().anchoredPosition = new Vector3(i * 110, j * 110, 0);
+                if (scores.Count >= counter + 1)
+                {
+                    temp.transform.FindChild("NameText").GetComponent<Text>().text =
+                        scores[counter].name;
+                    temp.transform.FindChild("ScoreText").GetComponent<Text>().text =
+                        scores[counter].score.ToString();
+                }
+                else
+                {
+                    temp.transform.FindChild("Name").GetComponent<Text>().text =
+                        "";
+                    temp.transform.FindChild("Score").GetComponent<Text>().text =
+                        "";
+                    temp.transform.FindChild("NameText").GetComponent<Text>().text =
+                        "";
+                    temp.transform.FindChild("ScoreText").GetComponent<Text>().text =
+                        "";
+                }
+                counter++;
+            }
+        }
+    }
+    public void closeHighScores()
+    {
+        foreach (Transform item in hsInputScreen.transform)
+        {
+            if (item.name == "Score(Clone)")
+            {
+                Destroy(item.gameObject);
+            }
+        }
+        hsInputScreen.SetActive(false);
     }
 
     public void close()
